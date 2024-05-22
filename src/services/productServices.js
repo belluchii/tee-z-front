05b58716e716763ead46bbc410c7978a62d5aab5
valentin <bellberger45@gmail.com>
2024-05-22 18:17:43 -0300

    url fix

--- src/services/productServices.js
+++ src/services/productServices.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getProducts = async () => {
   try {
-    return await axios.get(process.env.API_KEY + "/api/products");
+    return await axios.get("" + process.env.API_KEY + "/api/products");
   } catch (error) {
     console.error(error);
   }
@@ -10,7 +10,7 @@ export const getProducts = async () => {
 
 export const getOneProduct = async (name) => {
   try {
-    return await axios.get(process.env.API_KEY + "/api/products/" + name);
+    return await axios.get("" + process.env.API_KEY + "/api/products/" + name);
   } catch (error) {
     console.error(error);
   }
@@ -18,7 +18,9 @@ export const getOneProduct = async (name) => {
 
 export const getByTag = async (tag) => {
   try {
-    return await axios.get(process.env.API_KEY + "/api/products/tags/" + tag);
+    return await axios.get(
+      "" + process.env.API_KEY + "/api/products/tags/" + tag
+    );
   } catch (error) {
     console.error(error);
   }
@@ -26,7 +28,7 @@ export const getByTag = async (tag) => {
 
 export const reduceProduct = async (id, stock) => {
   try {
-    return await axios.put(process.env.API_KEY + "/api/products/" + id, {
+    return await axios.put("" + process.env.API_KEY + "/api/products/" + id, {
       stock: stock,
     });
   } catch (error) {
